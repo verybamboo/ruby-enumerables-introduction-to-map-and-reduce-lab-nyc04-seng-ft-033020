0d2a2139f73ed79b918ed5edd81e7340c27634b8
George Chen <georgechen1338@gmail.com>
2020-03-22 19:59:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # My Code here....
+def map_to_negative(source_array)
+  array = []
+  index = 0
+  
+  while index < source_array.length do
+    array.push(source_array[index] * -1)
+    index += 1
+  end
+  array
+end
